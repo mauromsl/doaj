60177d1758f4c7cca0d19607624de3c2542645e8
Emanuil Tolev <emanuil.tolev@gmail.com>
2014-10-10 09:37:15 +0100

    Refactor test helpers + add ability to spawn child flask app

--- doajtest/helpers.py
+++ doajtest/helpers.py
@@ -1,15 +1,61 @@
+import time
+import socket
+import subprocess
+import sys
+
 from unittest import TestCase
 from portality import core, dao
 from doajtest.bootstrap import prepare_for_test
-import time
 
 prepare_for_test()
 
+
 class DoajTestCase(TestCase):
     def setUp(self):
-        core.initialise_index(core.app)
-        time.sleep(1)
+        setup_index()
 
     def tearDown(self):
-        dao.DomainObject.destroy_index()
+        teardown_index()
+
+
+def setup_index():
+    core.initialise_index(core.app)
+    time.sleep(1)
+
+
+def teardown_index():
+    dao.DomainObject.destroy_index()
+    time.sleep(1)
+
+
+def get_first_free_port():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 0))  # let OS pick the first available port
+    free_port = sock.getsockname()[1]  # which port did the OS pick?
+    sock.close()
+    return free_port
+
+
+class LiveServer(object):
+    def __init__(self, port):
+        self.app = core.app
+        self.port = port
+        self._process = None
+
+    def get_server_url(self):
+        """
+        Return the url of the test server
+        """
+        return 'http://localhost:{0}'.format(self.port)
+
+    def spawn_live_server(self):
+        # sys.executable is the full, absolute path to the current Python interpreter
+        # This is used so that the new process with the test app in it runs properly in a virtualenv.
+        self._process = subprocess.Popen([sys.executable, 'portality/app.py', '--port', str(self.port)])
+
+        # we must wait for the server to start listening
         time.sleep(1)
+
+    def terminate_live_server(self):
+        if self._process:
+            self._process.terminate()
